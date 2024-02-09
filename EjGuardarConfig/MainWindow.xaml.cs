c22828409d5e4e99a5273af1e5e5c20f29104de5
Jon Barreras <77943625+ErJonyRL@users.noreply.github.com>
2024-02-09 18:12:48 +0100

    Update MainWindow.xaml.cs

--- EjGuardarConfig/MainWindow.xaml.cs
+++ EjGuardarConfig/MainWindow.xaml.cs
@@ -1,18 +1,27 @@
-﻿using System;
+using System;
 using System.Configuration;
 using System.IO;
 using System.Windows;
 
 namespace EjGuardarConfig
 {
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Constructor de la clase MainWindow.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
             CargarValores();
         }
 
+        /// <summary>
+        /// Método para cargar los valores de configuración en los cuadros de texto.
+        /// </summary>
         private void CargarValores()
         {
             // Obtener el valor del archivo de configuración clásico
@@ -24,6 +33,11 @@ namespace EjGuardarConfig
             TextoValorAppConfig.Text = valorAppConfig;
         }
 
+        /// <summary>
+        /// Método para leer el valor del archivo de configuración clásico.
+        /// Si el archivo no existe, se crea y se escribe un valor predeterminado.
+        /// </summary>
+        /// <returns>El valor leído o predeterminado.</returns>
         private string LeerValorArchivoConfiguracion()
         {
             string rutaArchivoConfiguracion = "config.txt";
@@ -35,6 +49,7 @@ namespace EjGuardarConfig
             }
             else
             {
+                // Si el archivo no existe, creamos uno nuevo y asignamos un valor predeterminado
                 valor = "Estoy en el MainWindows.xaml.cs";
                 File.WriteAllText(rutaArchivoConfiguracion, valor);
             }
@@ -42,6 +57,10 @@ namespace EjGuardarConfig
             return valor;
         }
 
+        /// <summary>
+        /// Método para obtener el valor del App.config.
+        /// </summary>
+        /// <returns>El valor obtenido del App.config.</returns>
         private string ObtenerValorAppConfig()
         {
             string valor = ConfigurationManager.AppSettings["MiClave"];
